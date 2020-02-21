c63642c4b1ce6bf27723b35c1b0b75c07813a97f
Aman Chauhan <ac247453@gmail.com>
2020-02-22 02:22:12 +0530

    first commit files added

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+        $('.bar.half').toggleClass('open');
+    });
+    $('.top-nav').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.bar.half').removeClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+    $('nav a[href*="#"]').on('click', function() {
+        $('html, body').animate(keyframes = {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, options = 2000);
+        $('.bar.half').removeClass('open');
+        // $('.menu-toggler').removeClass('open');
+
+    });
+});
